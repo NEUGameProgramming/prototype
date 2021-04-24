724a635d6de362cf9b142a01b7c0ee894dde802b
Hunter W <huntweng@gmail.com>
2021-04-23 22:39:14 -0400

    intro4

--- Assets/Scripts/CloseText.cs
+++ Assets/Scripts/CloseText.cs
@@ -11,6 +11,7 @@ public class CloseText : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        textToClose.SetActive(true);
         Time.timeScale = 0;
     }
 
